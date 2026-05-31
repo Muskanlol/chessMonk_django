0b6f17a46ffdccd3466923b3cc52937073014ce1
MuskanNag <nagmuskan68@gmail.com>
2026-05-31 15:46:09 +0530

    fix settings for Railway

--- chessmonk/settings.py
+++ chessmonk/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-NEW_RANDOM_STRING_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -171,5 +171,7 @@ STATICFILES_DIRS = [
 
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
